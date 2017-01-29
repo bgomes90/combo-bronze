cfea50c63df9f038bbbd516dbb307495191d1dd6
Bruno Gomes <Bruno Gomes>
2017-01-29 18:29:20 -0200

    ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var css = [
+  './assets/css/index.css'
+];
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require("gulp-concat");
+var cssmin = require("gulp-cssmin");
+var stripCssComments = require('gulp-strip-css-comments');
 
 gulp.task('sass', function () {
   return gulp.src('./assets/sass/**/*.scss')
@@ -9,6 +16,21 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('minify-css', function(){
+  gulp.src(css)
+      .pipe(stripCssComments({all: true}))
+      .pipe(concat('app.min.css'))
+      .pipe(cssmin())
+      .pipe(gulp.dest('./assets/css/'));
+});
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./assets/sass/**/*.scss', ['sass']);
+// });
+
+gulp.task('default',['sass', 'minify-css']);
+
+gulp.task('watch', function() {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  gulp.watch(css, ['minify-css']);
 });
